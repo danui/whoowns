1f10a5e6e2a6dbe27e708a09952c9ec72ff3a935
Jin <hsienjin@gmail.com>
2024-02-15 00:23:17 +0800

    MoreDomains - Add POSB and SCB

--- content-script.js
+++ content-script.js
@@ -1,23 +1,44 @@
 console.log("Start");
 console.log(window.location.host);
 
-function sendNotification(msg) {
-    console.log("Sending message: " + msg);
+function sendNotification(mapping) {
     chrome.runtime.sendMessage("", {
         type: "notification",
         options: {
             title: "Wh👀wns",
-            message: msg,
-            iconUrl: "eyes.png",
+            message: mapping.message,
+            iconUrl: mapping.logo,
             type: "basic"
         }
     });
     // alert(msg);
 }
 
+const mappings = [
+    {
+        domain: "dbs.com.sg",
+        message: "You are visiting DBS Singapore",
+        logo: "logos/dbs.png",
+    },
+    {
+        domain: "dbs.com",
+        message: "You are visiting DBS, the world's best bank",
+        logo: "logos/dbs.png",
+    },
+    {
+        domain: "posb.com.sg",
+        message: "You are visiting POSB",
+        logo: "logos/posb.png",
+    },
+    {
+        domain: "sc.com",
+        message: "You are visiting Standard Charted",
+        logo: "logos/scb.jpeg",
+    },
+];
 
-if (window.location.host.endsWith("dbs.com.sg")) {
-    sendNotification("You are visiting DBS Singapore");
-} else if (window.location.host.endsWith("dbs.com")) {
-    sendNotification("You are visiting the world's best bank DBS");
-}
+mappings.forEach((mapping) => {
+    if (window.location.host.endsWith(mapping.domain)) {
+        sendNotification(mapping);
+    }
+})
--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,9 @@
 chrome.runtime.onMessage.addListener(data => {
     console.log(data);
     if (data.type === "notification") {
-        console.log("Creating notification");
         chrome.notifications.create("", data.options, (notificationId) => {
             console.log("notificationId:", notificationId);
             console.log("Last error:", chrome.runtime.lastError);    
         });
-        console.log("CHECKPOINT-188")
-        if (chrome.runtime.lastError) {
-            console.error(chrome.runtime.lastError);
-        }
     }
 });
